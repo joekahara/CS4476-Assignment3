13cdbb58dd0a94632a3c4d2cc892a94dfa3c8504
Joseph Kahara <joekahara@gmail.com>
2019-11-04 22:49:49 -0500

    Step 1 of hash implemented

--- src/Hashing.kt
+++ src/Hashing.kt
@@ -1,15 +1,24 @@
 fun hash(str: String): String {
     val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
-    var result = StringBuilder()
-    val blocks = str.chunked(25)
-    val matrices = blocks.map { it.chunked(5) }
-    println(blocks.toString())
-    println(matrices.toString())
+    val numericResult = mutableListOf(0,0,0,0,0) // initialize empty list
 
-    return result.toString()
+    val nums = str.toUpperCase().padEnd(str.length+(5-str.length%5), ' ').map { letters.indexOf(it) }
+    if (!nums.all{ it in 0..26 }) return "Please enter a valid plaintext string (only alphabetic & space). "
+    val matrices = nums.chunked(5).chunked(5)
+    matrices.iterator().forEach { matrix ->
+        matrix.iterator().forEach {
+            for (i in 0 until 5)
+                numericResult[i] += it[i]
+        }
+    }
+
+    
+
+    return numericResult.map { letters[it%27] }.toString()
 }
 
 fun main() {
-    val plaintext = "this is the plaintext used for hashing in this assignment i am testing and wondering how many characters are in this string"
+    val plaintext = "abcdefghi jklmnopqrstuvw"
     val result = hash(plaintext)
+    println(result)
 }
