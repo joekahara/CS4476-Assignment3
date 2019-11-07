eab2c8c00d05096b1d538008bfdb8cd8bd6b271e
Joseph Kahara <joekahara@gmail.com>
2019-11-07 14:07:30 -0500

    Implemented round 1 & 2 of hash function

--- src/Hashing.kt
+++ src/Hashing.kt
@@ -1,24 +1,32 @@
+import java.util.*
+
 fun hash(str: String): String {
     val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
-    val numericResult = mutableListOf(0,0,0,0,0) // initialize empty list
+    var numericResult = mutableListOf(0,0,0,0,0) // initialize empty list for results
 
-    val nums = str.toUpperCase().padEnd(str.length+(5-str.length%5), ' ').map { letters.indexOf(it) }
-    if (!nums.all{ it in 0..26 }) return "Please enter a valid plaintext string (only alphabetic & space). "
+    val nums = str.toUpperCase().padEnd(if (str.length%5 == 0) 0 else (str.length+(5-str.length%5)), ' ').map { letters.indexOf(it) }
+    if (!nums.all{ it in 0..26 }) return "Please enter a valid plaintext string (only alphabetic & space). "     // I/O error checking
     val matrices = nums.chunked(5).chunked(5)
-    matrices.iterator().forEach { matrix ->
-        matrix.iterator().forEach {
-            for (i in 0 until 5)
-                numericResult[i] += it[i]
+    matrices.forEach { matrix ->
+        matrix.forEachIndexed { chunkIndex, chunkOfFive ->
+
+            // round 1
+            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num }
+
+            // round 2
+            if (chunkIndex < 4) Collections.rotate(chunkOfFive, -chunkIndex-1)
+            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num }
+
+            // round 3
+
         }
     }
 
-    
-
-    return numericResult.map { letters[it%27] }.toString()
+    return numericResult.map { letters[it % 27] }.toString()
 }
 
 fun main() {
-    val plaintext = "abcdefghi jklmnopqrstuvw"
+    val plaintext = "abcdefghi jklmnopqrstuvwx"
     val result = hash(plaintext)
     println(result)
 }
