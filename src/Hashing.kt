89f628c965630bcf8ca89f68c38bc47e291608a7
Joseph Kahara <joekahara@gmail.com>
2019-11-11 16:42:53 -0500

    Final edits
    Changed numOfVariations in birthday attack and included code comments for readability

--- src/Hashing.kt
+++ src/Hashing.kt
@@ -1,6 +1,4 @@
 import java.util.*
-import kotlin.math.pow
-import kotlin.math.roundToInt
 import kotlin.random.Random
 
 const val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
@@ -14,26 +12,26 @@ fun hash(str: String): String {
 
     val round3Matrix = List(5) { MutableList(5) {0} }
 
-    matrices.forEach { matrix ->
-        matrix.forEachIndexed { chunkIndex, chunkOfFive ->
+    matrices.forEach { matrix -> // iterate through 25 character blocks
+        matrix.forEachIndexed { chunkIndex, chunkOfFive -> // iterate through each row of the current 5x5 matrix
 
             // round 1
-            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num }
+            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num } // add each element of the matrix row to the proper result element
 
             // round 2
-            if (chunkIndex < 4) Collections.rotate(chunkOfFive, -(chunkIndex+1))
-            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num }
+            if (chunkIndex < 4) Collections.rotate(chunkOfFive, -(chunkIndex+1)) // for all rows with index less than 4, rotate i times left
+            chunkOfFive.mapIndexed { index, num -> numericResult[index] += num } // add to result similarly to round 1
 
             // round 3
             chunkOfFive.mapIndexed { index, num ->
-                round3Matrix[index][chunkIndex] = num
+                round3Matrix[index][chunkIndex] = num // copy values from original matrix to new rotated matrix so that rows are now columns
             }
         }
 
         // round 3 continued
         round3Matrix.forEachIndexed { chunkIndex, chunk ->
-            if (chunkIndex < 4) Collections.rotate(chunk, chunkIndex+1)
-            chunk.mapIndexed { index, num -> numericResult[index] += num }
+            if (chunkIndex < 4) Collections.rotate(chunk, chunkIndex+1) // rotate all rows with index less than 4 of new matrix i times left
+            chunk.mapIndexed { index, num -> numericResult[index] += num } // add to result similarly to other steps
         }
     }
 
@@ -44,30 +42,29 @@ fun mac(str: String, key: String): String {
     return hash(key + hash(key + str))
 }
 
-fun birthdayAttack(digestLength: Int): String {
-    val total = 2f.pow(digestLength/2f).roundToInt()
-    val testsAndResults = mutableMapOf<String,String>()
+fun birthdayAttack(numOfVariations: Int): String {
+    val testsAndResults = mutableMapOf<String,String>() // create empty mutable map to store hash inputs and outputs
 
-    for (i in 0..total) {
+    for (i in 0..numOfVariations) { // hash 'numOfVariations' random strings of 50 characters, store the input and output
         val randomString = List(50) { letters[Random.nextInt(0, letters.length)] }.joinToString("")
         testsAndResults[randomString] = hash(randomString)
     }
 
-    for (i in 0..total) {
+    for (i in 0..numOfVariations) { // hash 'numOfVariations' random strings, compare each with previous variations to find collisions
         val randomString = List(50) { letters[Random.nextInt(0, letters.length)] }.joinToString("")
         val randomHash = hash(randomString)
-        if (testsAndResults.values.any { it == randomHash })
-            return "Collision found with inputs [$randomString], ${testsAndResults.filterValues { it == randomHash }.keys} and output [$randomHash]"
+        if (testsAndResults.values.any { it == randomHash }) // if any collisions, return results as string
+            return "Collision found with inputs [$randomString], ${testsAndResults.filterValues { it == randomHash }.keys} and output [$randomHash] using birthday attack. "
     }
 
-    return "Birthday attack unsuccessful with 2^(2/m) = $total variations used. "
+    return "Birthday attack was unsuccessful with $numOfVariations variations used. " // if no collisions for all variations, return appropriate string
 }
 
 fun main() {
     val plaintext = "the birthday attack can be performed for any hash functions including sha three"
     val result = hash(plaintext)
-    println(result)
+    println("Hash output: $result")
     val macResult = mac(plaintext, "abcde")
-    println(macResult)
-    println(birthdayAttack(5))
+    println("MAC output: $macResult")
+    println(birthdayAttack(2500))
 }
